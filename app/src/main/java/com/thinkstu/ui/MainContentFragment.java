4515d815b9de054f1fc6432791d66df4a1f32bdb
ThinkStu <1914493943@qq.com>
2023-07-03 10:53:44 +0800

    Default Changelist

---
+++ app/src/main/java/com/thinkstu/ui/MainContentFragment.java
@@ -0,0 +1,94 @@
+package com.thinkstu.ui;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.thinkstu.MainActivity;
+import com.thinkstu.Service.SongSheetService;
+import com.thinkstu.Service.Impl.SongSheetServiceImpl;
+import com.thinkstu.adater.SongSheetAdapter;
+import com.thinkstu.dto.SongDto;
+import com.thinkstu.entity.SongBean;
+import com.thinkstu.entity.SongSheetBean;
+import com.thinkstu.music.*;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.List;
+
+/*歌单列表*/
+
+public class MainContentFragment extends Fragment {
+    private static final String TAG = "MainContentFragment";
+
+    private View view;
+    private static MainContentFragment mainContentFragment;
+    private SongSheetService songSheetService;
+
+    public static MainContentFragment getInstance() {
+        if (mainContentFragment == null) {
+            synchronized (MainContentFragment.class) {
+                if (mainContentFragment == null) {
+                    mainContentFragment = new MainContentFragment();
+                }
+            }
+        }
+        return mainContentFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_main_content, container, false);
+        songSheetService = new SongSheetServiceImpl();
+        initView();
+        return view;
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if (!hidden) {
+            Log.d(TAG, "onHiddenChanged: show");
+            initView();
+        }
+    }
+
+    private void initView() {
+        //查歌单
+        ListView listView = view.findViewById(R.id.main_listView_songSheet);
+        final List<SongSheetBean> data = songSheetService.findAll();
+        final SongSheetAdapter songSheetAdapter = new SongSheetAdapter(getContext(), data);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final View view1 = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_create_sheet, null);
+        listView.setAdapter(songSheetAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                MainActivity mainActivity = (MainActivity) getActivity();
+                if (mainActivity != null) {
+                    SongSheetBean songSheetBean = (SongSheetBean) adapterView.getItemAtPosition(i);
+                    List<SongBean> songBeanList = songSheetService.findSongBeanBySongSheetId(songSheetBean.getId());
+                    if (i!=0) {
+                        EventBus.getDefault().postSticky(new SongDto(songSheetBean, songBeanList));
+                    } else {
+                        EventBus.getDefault().postSticky(new SongDto(songSheetBean, songBeanList, true));
+                    }
+                    mainActivity.enterSongContentFragment();
+                }
+            }
+        });
+
+    }
+
+}
